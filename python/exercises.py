c62632d7d32a838b4496cc70354b34940bc5e0a1
moshelederman <mos9972@gmail.com>
2024-11-11 23:31:04 +0200

    continu

--- python/exercises.py
+++ python/exercises.py
@@ -120,4 +120,78 @@ list3 = list1 + list2
 print(list3)
 
 #exerice 20
-enumbers = [1, 2, 2, 3, 4, 2]
+
+def remove_all(lst, value):
+    while value in lst:
+        lst.remove(value)
+    return lst
+numbers = [1, 2, 2, 3, 4, 2]
+print(numbers)
+remove_all(numbers, 2)
+print(numbers)
+
+#exerice 21
+
+def bubble_sort(lst):
+    n = len(lst)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+    return lst
+numbers = [64, 34, 25, 12, 22, 11, 90]
+sorted_numbers = bubble_sort(numbers)
+print(sorted_numbers)
+
+#Tuples
+#1
+
+my_tuple = (1, 2, 3)
+print(my_tuple)
+print(my_tuple[1])
+#my_tuple[0] = "10"
+#print(my_tuple)
+
+#2
+person = ("MOSHE_LEDERMAN", "29", "BB")
+#print(person)
+(name, age, city) = person
+print(name)
+print(age)
+print(city)
+
+#3
+
+nested_tuple =((1, 2, 3), (4, 5, 6))
+five_nested_tuple = nested_tuple[1][1]
+print(five_nested_tuple)
+
+#4
+
+Tnumbers = (1, 2, 3, 2, 4, 2)
+print(Tnumbers.count(2))
+print(Tnumbers.index(3))
+
+#Dictionaries
+#1
+
+student = {
+    "name": "moshe",
+    "age": "29",
+    "grade": "1"
+}
+print(student["name"])
+student["school"] = "YRG"
+print(student)
+
+#2
+student["age"] = 30
+print(student)
+student.pop("grade")
+print(student)
+
+#3
+capitals = {'France': 'Paris','Spain': 'Madrid','Japan': 'Tokyo'}
+for county, capital in capitals.items():
+    print(f"The capital of {county} is {capital}")
+    
