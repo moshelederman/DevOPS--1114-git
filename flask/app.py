094d1d8a9d19d10bb21473a4e878e3bbf1d71bab
moshelederman <mos9972@gmail.com>
2024-11-19 14:46:27 +0200

    flask1

--- flask/app.py
+++ flask/app.py
@@ -1,1 +1,16 @@
 # a new project
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/greet/<name>")
+def greet_fun(name):
+    return f"<p>Hello, {name}!</p>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
