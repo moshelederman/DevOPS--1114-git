8cf01c815588d1c7f1aba1fcc775943ec3bf0dbd
moshelederman <mos9972@gmail.com>
2024-12-31 11:29:33 +0200

    new change

--- GitHubActions/test_calculator.py
+++ GitHubActions/test_calculator.py
@@ -1,7 +1,7 @@
 # test_calculator.py
 
 import pytest
-from calculator import add, subtract, multiply, divide
+from Calculator import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
