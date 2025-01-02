fe3f541148c08357b899f0d2fe14c875e1adcd51
moshelederman <mos9972@gmail.com>
2025-01-02 11:20:36 +0200

    some changes

--- Weather App/app.py
+++ Weather App/app.py
@@ -31,4 +31,4 @@ def index():
     return render_template("index.html", weather=weather_data, facts=winter_facts)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
